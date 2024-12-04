b055d133a86148e6a62b5dd940fe93c0d9374147
aitpaeva00 <aizhan.aitpaeva@alatoo.edu.kg>
2024-12-05 00:31:30 +0600

    Added new task list design and functionality

--- task-manager/src/TaskList.jsx
+++ task-manager/src/TaskList.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 import useTaskManager from './hooks/useTaskManager';  
 
 const TaskList = () => {
@@ -15,18 +16,25 @@ const TaskList = () => {
   return (
     <div>
       <h2>Task List</h2>
+      
+      {/* Input field for adding a new task */}
       <input
         type="text"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
+        value={newTask}  // The value of the input field is controlled by state
+        onChange={(e) => setNewTask(e.target.value)}  // Update state on input change
         placeholder="Enter a new task"
       />
+      
+      {/* Button to trigger adding the task */}
       <button onClick={handleAddTask}>Add Task</button>
 
+      {/* Displaying the list of tasks */}
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>
-            {task.text}
+            {task.text}  {/* Display the task text */}
+            
+            {/* Button to delete a task */}
             <button onClick={() => removeTask(task.id)}>Delete</button>
           </li>
         ))}
